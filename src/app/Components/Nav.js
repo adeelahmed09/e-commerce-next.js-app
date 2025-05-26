6d0fba112098d0d65be2506f1421af860e32290d
adeelahmed9 <adeel0ahmed10@gmail.com>
2025-05-26 14:01:57 +0500

    create home hero section and profile UI

---
+++ src/app/Components/Nav.js
@@ -0,0 +1,100 @@
+"use client"
+import 'remixicon/fonts/remixicon.css'
+import Link from "next/link"
+import gsap from 'gsap'
+import { useEffect, useRef, useState } from 'react'
+import { useGSAP } from '@gsap/react'
+
+function Nav() {
+  gsap.registerPlugin()
+  const [indexMenue, setIndexMenue] = useState(0)
+  const menu = useRef(null)
+  const cancel = useRef(null)
+  const menuNav = useRef(null)
+  const pcNav = useRef(null)
+  const menuNavBack = useRef(null)
+  const onClickMenuHandler = () => {
+    if (indexMenue == 0) {
+      gsap.to(menuNav.current, {
+        width: "70%",
+        duration: .2,
+      })
+      setIndexMenue(1);
+      gsap.to(menu.current, {
+        fontSize: 0,
+        duration: 0.1,
+      })
+      gsap.to(cancel.current, {
+        fontSize: 36,
+        duration: 0.1,
+        delay: 0.1,
+        opacity: 100
+      })
+    }
+  }
+  const onClickCancel = () => {
+    if (indexMenue == 1) {
+      gsap.to(menuNav.current, {
+        width: "0%",
+        duration: .2,
+      })
+      setIndexMenue(1);
+      gsap.to(menu.current, {
+        fontSize: 36,
+        duration: 0.1,
+        delay: 0.1,
+      })
+      gsap.to(cancel.current, {
+        fontSize: 0,
+        duration: 0.1,
+
+        opacity: 0
+      })
+      setIndexMenue(0)
+    }
+  }
+  useGSAP(function () {
+    gsap.from(pcNav.current, {
+      y: -100,
+      duration: .5,
+      opacity: 0,
+      delay: .5,
+    })
+  })
+  return (
+    <>
+      <div ref={pcNav} className="w-full sm:flex  hidden justify-center items-center z-20 pt-[2vw] fixed top-0">
+        <div className=" px-[5vw] w-[60vw] py-6 items-center flex justify-between bg-white/10 backdrop-blur-md  rounded-[50px]">
+          <div className=" text-3xl font-bold text-[#0A84FF]">
+            <h1> Logo</h1>
+          </div>
+          <div className="flex tex-lg text-[#e6e6e6] font-semibold gap-5">
+            <Link href={"/"} className="hover:text-blue-400 hover:scale-110 transition">Home</Link>
+            <Link href={"#"} className="hover:text-blue-400 hover:scale-110 transition">About Us</Link>
+            <Link href={"#"} className="hover:text-blue-400 hover:scale-110 transition">Contact Us</Link>
+          </div>
+        </div>
+        <div className=' absolute right-12 text-2xl px-3 py-2 rounded-full bg-white/10 backdrop-blur-md   font-thin text-[#0A84FF] '>
+           <Link href={"../Pages/Profile"}><i className="ri-user-line"></i></Link>
+        </div>
+      </div>
+      <div className=" fixed top-0 sm:hidden px-4 z-30   w-full pt-4">
+        <i ref={menu} onClick={onClickMenuHandler} className="ri-menu-3-line  absolute right-5 text-4xl text-[#0A84FF]"></i>
+        <i ref={cancel} onClick={onClickCancel} className=" text-[#0A84FF]  absolute right-5  text-0 opacity-0 ri-close-large-line"></i>
+        <div className=' absolute left-5 text-2xl px-3 py-2 rounded-full bg-white/10 backdrop-blur-md   font-thin text-[#0A84FF] '>
+           <Link href={"../Pages/Profile"}><i className="ri-user-line"></i></Link>
+        </div>
+      </div>
+      
+      <div ref={menuNavBack} className='sm:hidden flex justify-end fixed  z-20 w-screen  h-screen'>
+        <div ref={menuNav} className='w-[0%] overflow-hidden  text-2xl gap-5 font-semibold bg-white/10 backdrop-blur-md  text-[#e6e6e6] z-40 flex flex-col justify-center items-center h-screen'>
+          <Link href={"/"} onClick={onClickCancel} className="hover:text-blue-400 w-[70vw] text-center hover:scale-110 transition">Home</Link>
+          <Link href={"#"} onClick={onClickCancel} className="hover:text-blue-400 w-[70vw] text-center hover:scale-110 transition">About Us</Link>
+          <Link href={"#"} onClick={onClickCancel} className="hover:text-blue-400 w-[70vw] text-center hover:scale-110 transition">Contact Us</Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Nav
---
+++ src/app/Pages/Profile/page.js
@@ -0,0 +1,21 @@
+"use client"
+
+import Link from "next/link"
+import React, { useState } from "react"
+
+function page() {
+    const [signUp, setSignUp] = useState(false)
+    if (!signUp) {
+        return (
+            <div className="w-full flex flex-col gap-5 justify-center items-center  h-screen">
+                <h1 className="text-3xl text-[#e6e6e6] font-[space] sm:w-fit w-[80vw] text-center">You Are Not Logged Please Login</h1>
+                <div className="flex items-center justify-center gap-5">
+                    <Link href={"#"} className="text-xl text-[#e6e6e6] bg-[#0A84FF] font-semibold px-3 py-2 rounded-lg">Sign Up</Link>
+                    <Link href={"#"} className="text-xl text-[#e6e6e6] bg-[#0A84FF] font-semibold px-3 py-2 rounded-lg">Log In</Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default page
