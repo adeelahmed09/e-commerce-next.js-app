03b937d76d6b5731f20c343659c1a1feef82a40c
adeelahmed9 <adeel0ahmed10@gmail.com>
2025-06-14 07:31:40 +0500

    test

--- src/app/Pages/product/page.js
+++ src/app/Pages/product/page.js
@@ -3,9 +3,26 @@ import { Suspense } from "react";
 
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading product...</div>}>
-      <ProductPage/>
-    </Suspense>
-  )
+    const { data: session, status } = useSession();
+    const [userDetails, setUserDetails] = useState(
+        {
+
+        }
+    )
+    useEffect(() => {
+        if (status === "authenticated") {
+            setUserDetails({
+                name: session.user.name,
+                email: session.user.email,
+                avatar: session.user.avatar,
+                role: session.user.role,
+                id: session.user.id
+            })
+        }
+    }, [status]);
+    return (
+        <Suspense fallback={<div>Loading product...</div>}>
+            <ProductPage />
+        </Suspense>
+    )
 }
